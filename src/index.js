c902b3e2cf53723db85eab02a56f752da4f7a476
methodius pkemoi <pkemoimethodius@gmail.com>
2024-07-10 13:02:52 +0300

    Adds the changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ document.getElementById("create-task-form").addEventListener("submit",(e) =>{
+e.preventDefault()
+handleList(e.target.tasks.value)
+
+ })
+})
+function handleList(item){
+let li=document.createElement ("li")
+li.textContent=`${item}`
+document.getElementById("tasks").appendChild("li")
+console.log(li)
+}
